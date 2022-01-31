992cf0c264a22e1dcf2b2815b8a3bf10c29032b6
bcosters <ctw01914@criticaltechworks.com>
2022-01-31 10:34:28 +0000

    Corrected 09/ex02

--- module09_extras/ex02/main/copy_vs_move.cpp
+++ module09_extras/ex02/main/copy_vs_move.cpp
@@ -5,28 +5,65 @@
 #include "copy_swap.hpp"
 #include "move_swap.hpp"
 
-int main () {
-    int a = 21;
-    int b = 42;
-    std::string c = "LOL";
-    std::string d = "LMAO";
+struct Num {
+    int n;
+    Num(int const & n) : n(n) {}
+    Num(Num const & N) : n(N.n) {
+        std::cout << "Copy ctor.\n";
+    }
+    Num(Num && N) : n(std::move(N.n)) {
+        std::cout << "Move ctor.\n";
+    }
+    Num &   operator=(Num const & rhs) = default;
+    int const & getNum() const { return n; }
+};
 
-    std::cout << a << '\n';
-    std::cout << b << '\n';
-    std::cout << c << '\n';
-    std::cout << d << '\n';
+struct Str {
+    std::string str;
+    Str(std::string const & s) : str(s) {}
+    Str(Str const & S) : str(S.str) {
+        std::cout << "Copy ctor.\n";
+    }
+    Str(Str && S) : str(std::move(S.str)) {
+        std::cout << "Move ctor.\n";
+    }
+    Str &   operator=(Str const & rhs) = default;
+    std::string const & getStr() const { return str; }
+};
+
+int main () {
+    Num a = 21;
+    Num b = 42;
+    Str c("LOL");
+    Str d("LMAO");
+
+    std::cout << "Before copy swap\n";
+    std::cout << a.getNum() << '\n';
+    std::cout << b.getNum() << '\n';
+    std::cout << c.getStr() << '\n';
+    std::cout << d.getStr() << '\n';
     copySwap(a, b);
     copySwap(c, d);
-    std::cout << a << '\n';
-    std::cout << b << '\n';
-    std::cout << c << '\n';
-    std::cout << d << '\n';
-    int e = std::move(a) + std::move(b);
-    std::string f = std::move(c) + std::move(d);
-    std::cout << a << '\n';
-    std::cout << b << '\n';
-    std::cout << c << '\n';
-    std::cout << d << '\n';
-    std::cout << e << '\n';
-    std::cout << f << '\n';
+    std::cout << "After copy swap\n";
+    std::cout << a.getNum() << '\n';
+    std::cout << b.getNum() << '\n';
+    std::cout << c.getStr() << '\n';
+    std::cout << d.getStr() << '\n';
+    std::cout << "Before move swap\n";
+    moveSwap(a, b);
+    moveSwap(c, d);
+    std::cout << "After move swap\n";
+    std::cout << a.getNum() << '\n';
+    std::cout << b.getNum() << '\n';
+    std::cout << c.getStr() << '\n';
+    std::cout << d.getStr() << '\n';
+    std::cout << "After move swap\n";
+    Num e(std::move(a));
+    Str f(std::move(c));
+    std::cout << a.getNum() << '\n';
+    std::cout << b.getNum() << '\n';
+    std::cout << c.getStr() << '\n';
+    std::cout << d.getStr() << '\n';
+    std::cout << e.getNum() << '\n';
+    std::cout << f.getStr() << '\n';
 }
